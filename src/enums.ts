2b11058ecf9459ce954ad95df8bb87b01cea1388
haris akbar <harisakbar04@gmail.com>
2021-03-30 16:21:56 +0700

    add enums initiate value

--- src/enums.ts
+++ src/enums.ts
@@ -1,5 +1,8 @@
 // zero index
-enum ResourceType { BOOK, AUTHOR, FILM, PERSON, DIRECTOR }
+// enum ResourceType { BOOK, AUTHOR, FILM, PERSON, DIRECTOR }
+// in enum can add default value
+// enum ResourceType { BOOK = 2, AUTHOR, FILM, PERSON, DIRECTOR } // will auto increment from 2 index
+enum ResourceType { BOOK = 2, AUTHOR, FILM = 5, PERSON = 'person', DIRECTOR = 2 } // if person default string, have to add initiate value for director to
 
 interface ResourceVideo<T> {
   uid: number;
